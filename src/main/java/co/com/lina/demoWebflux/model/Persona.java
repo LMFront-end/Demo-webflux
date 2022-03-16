8eda60a2261ade11b7141e10d45de29b72533376
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-15 20:10:21 -0500

    se crea el modelo Persona

---
+++ src/main/java/co/com/lina/demoWebflux/model/Persona.java
@@ -0,0 +1,14 @@
+package co.com.lina.demoWebflux.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Persona {
+
+    private Integer idPersona;
+    private String nombre;
+}
