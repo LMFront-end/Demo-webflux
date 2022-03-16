b9530cf7f28b546e660ff302ceae1aea5e5167da
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-15 21:18:42 -0500

    se hace simulacion de base de datos

--- src/main/java/co/com/lina/demoWebflux/controller/PersonaController.java
+++ src/main/java/co/com/lina/demoWebflux/controller/PersonaController.java
@@ -4,9 +4,10 @@ package co.com.lina.demoWebflux.controller;
 import co.com.lina.demoWebflux.model.Persona;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -20,7 +21,7 @@ public class PersonaController {
 
     private static final Logger log = LoggerFactory.getLogger(PersonaController.class);
 
-    //crear
+
     @GetMapping("mostrar")
     // Retornar un flujo de datos de una sola persona
     public Mono<Persona> mostrar(){
@@ -28,15 +29,54 @@ public class PersonaController {
         return Mono.just(new Persona(1, "Lina Maria"));
     }
 
-    @GetMapping
+    @GetMapping("")
     public Flux<Persona> listar(){
         List<Persona> personas = new ArrayList<>();
 
         personas.add(new Persona(1, "Juan Pablo"));
         personas.add(new Persona(1, "Diana"));
 
-        Flux <Persona> personasFlux = Flux.fromIterable(personas);
-
-        return personasFlux;
+        return Flux.fromIterable(personas);
     }
+
+    @GetMapping("/response")
+    // devolver un flujo de personas
+    public Mono<ResponseEntity<Flux<Persona>>> listarEntity(){
+        List<Persona> personas = new ArrayList<>();
+
+        personas.add(new Persona(1, "Juan Pablo"));
+        personas.add(new Persona(1, "Diana"));
+
+        Flux<Persona> personaFlux =  Flux.fromIterable(personas);
+
+        // Enfoque asncrono --> libreria de webFlux
+        return Mono.just(ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON).body(personaFlux)
+        );
+    }
+
+    // simulación de una base de datos
+    @DeleteMapping("/{modo}")
+    public Mono<ResponseEntity<Void>> eliminar(@PathVariable("modo") Integer modo) {
+        return buscarPersona(modo)
+                // el operador flapMap --> Retornar otro flujo de datos
+                .flatMap(p -> {
+                    return eliminar(p)
+                            // retornar algo vacio HttpStatus.NO_CONTENT
+                            .then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
+                });
+    }
+
+    public Mono<Void> eliminar(Persona p){
+        log.info("Eliminando " + p.getIdPersona() + " = " + p.getNombre());
+
+        return Mono.empty();
+    }
+
+    public Mono<Persona> buscarPersona(Integer mono){
+        if (mono == 1){
+            return Mono.just(new Persona(1, "Lina"));
+        }else return Mono.empty();
+    }
+
 }
