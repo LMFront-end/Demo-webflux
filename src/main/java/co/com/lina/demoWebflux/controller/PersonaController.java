b19c480b6130a8d1afcbf33c9ac11b8acc44c2c8
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-15 20:34:51 -0500

    se crea ejemplo para Mono y Flux

--- src/main/java/co/com/lina/demoWebflux/controller/PersonaController.java
+++ src/main/java/co/com/lina/demoWebflux/controller/PersonaController.java
@@ -7,7 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -20,6 +24,19 @@ public class PersonaController {
     @GetMapping("mostrar")
     // Retornar un flujo de datos de una sola persona
     public Mono<Persona> mostrar(){
+        // Mono.just --> retornar un Mono de personas
         return Mono.just(new Persona(1, "Lina Maria"));
+    }
+
+    @GetMapping
+    public Flux<Persona> listar(){
+        List<Persona> personas = new ArrayList<>();
+
+        personas.add(new Persona(1, "Juan Pablo"));
+        personas.add(new Persona(1, "Diana"));
+
+        Flux <Persona> personasFlux = Flux.fromIterable(personas);
+
+        return personasFlux;
     }
 }
