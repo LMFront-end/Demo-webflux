1ad765580c6cc1c819f37a3a79d4f88c9b3066e4
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-15 20:22:40 -0500

    se prueba ruta en postman

--- src/main/java/co/com/lina/demoWebflux/controller/PersonaController.java
+++ src/main/java/co/com/lina/demoWebflux/controller/PersonaController.java
@@ -1,10 +1,25 @@
 package co.com.lina.demoWebflux.controller;
 
 
+import co.com.lina.demoWebflux.model.Persona;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/personas/")
 public class PersonaController {
+
+    private static final Logger log = LoggerFactory.getLogger(PersonaController.class);
+
+    //crear
+    @GetMapping("mostrar")
+    // Retornar un flujo de datos de una sola persona
+    public Mono<Persona> mostrar(){
+        return Mono.just(new Persona(1, "Lina Maria"));
+    }
 }
